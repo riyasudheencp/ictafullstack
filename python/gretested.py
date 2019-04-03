0ca901ea1e3890b86d0a3c4cc0138ee1650ba225
riyasudheencp <riyasudheencp04@gmail.com>
2019-04-03 16:57:34 +0530

    pythonsecond

---
+++ python/gretested.py
@@ -0,0 +1,8 @@
+list=[]
+num=int(input("enter how many number:"))
+for n in range(num):
+	numbers=int(input("enter the number"))
+	list.append(numbers)
+print list
+print max(list)
+print min(list)
---
+++ python/listing.py
@@ -0,0 +1,13 @@
+a=[1,2,3]
+b=[4,5,6]
+
+
+a.extend(b)
+
+print a
+a.insert(2,7)
+print a
+a.pop()
+print a
+a.pop(3)
+print a
